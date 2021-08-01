5647ef7e4e9f247bcfcd2269755a144cf0b92f6f
gis3dts <79392405+gis3dts@users.noreply.github.com>
2021-08-01 13:13:12 -0400

    Update main.js

--- main.js
+++ main.js
@@ -12,3 +12,26 @@ function random(min, max) {
   const num = Math.floor(Math.random() * (max - min + 1)) + min;
   return num;
 }
+
+function random(min, max) {
+  const num = Math.floor(Math.random() * (max - min + 1)) + min;
+  return num;
+}
+
+function Ball(x, y, velX, velY, color, size) {
+  this.x = x;
+  this.y = y;
+  this.velX = velX;
+  this.velY = velY;
+  this.color = color;
+  this.size = size;
+}
+
+Ball.prototype.draw = function() {
+  ctx.beginPath();
+  ctx.fillStyle = this.color;
+  ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+  ctx.fill();
+}
+
+let testBall = new Ball(50, 100, 4, 4, 'blue', 10);
