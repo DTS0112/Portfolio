8816bf91b0031b8278f36dd0addc41774837f219
gis3dts <79392405+gis3dts@users.noreply.github.com>
2021-08-04 10:31:50 -0400

    Update nixpix.js

--- javascript/nixpix.js
+++ javascript/nixpix.js
@@ -1,7 +1,24 @@
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
-const image = document.getElementById('source');
+// const canvas = document.getElementById('canvas');
+// const ctx = canvas.getContext('2d');
+// const image = document.getElementById('source');
 
-image.addEventListener('load', e => {
-  ctx.drawImage(image, 33, 71, 104, 124, 21, 20, 87, 104);
-});
+// image.addEventListener('load', e => {
+//   ctx.drawImage(image, 33, 71, 104, 124, 21, 20, 87, 104);
+// });
+
+
+var dx = 10;
+    var dy = 20;
+    var dWidth = 50;
+    var dHeight = 60;
+     
+  
+  var ctx = document.getElementById('canvas').getContext('2d');
+  var img = new Image();
+  img.addEventListener('load', function(){
+    ctx.drawImage(img, 0, 0);
+  });
+  img.src = 'assets/images/NixPic.jpg';
+ 
+      
+ 
