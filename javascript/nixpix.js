6e84d940400933bca271bb64ac949640ae35db7d
gis3dts <79392405+gis3dts@users.noreply.github.com>
2021-08-04 11:07:44 -0400

    Update nixpix.js

--- javascript/nixpix.js
+++ javascript/nixpix.js
@@ -12,12 +12,12 @@ var nixdx = 10;
     var nixdWidth = 50;
     var nixdHeight = 60;
      
-   const nixcanvas = document.querySelector('nixcanvas');
-   const nixctx = nixcanvas.getContext('2d');
+   const nixoncanvas = document.querySelector('nixcanvas');
+   const nixonctx = nixoncanvas.getContext('2d');
  
   var img = new Image();
   img.addEventListener('load', function(){
-    nixctx.drawImage(img, 0, 0);
+    nixonctx.drawImage(img, 0, 0);
   });
   img.src = '../assets/images/NixPic.jpg';
  
