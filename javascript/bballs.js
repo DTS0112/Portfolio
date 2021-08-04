73df618755f57318e958c155e706a5c81f96011f
gis3dts <79392405+gis3dts@users.noreply.github.com>
2021-08-04 11:24:07 -0400

    Update bballs.js

--- javascript/bballs.js
+++ javascript/bballs.js
@@ -3,8 +3,10 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
-const width = canvas.width = window.innerWidth;
-const height = canvas.height = window.innerHeight;
+const width = canvas.width = "30";
+const height = canvas.height = "30";
+//const width = canvas.width = window.innerWidth;
+//const height = canvas.height = window.innerHeight;
 
 // function to generate random number
 
