98d18d730dfeb70019f7a451d1af6f818ba41b4c
gis3dts <79392405+gis3dts@users.noreply.github.com>
2021-08-04 11:36:06 -0400

    Update bballs.js

--- javascript/bballs.js
+++ javascript/bballs.js
@@ -3,8 +3,8 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
-const width = canvas.width = '300';
-const height = canvas.height = '300';
+const width = canvas.width = "100";
+const height = canvas.height = "100";
 //const width = canvas.width = window.innerWidth;
 //const height = canvas.height = window.innerHeight;
 
